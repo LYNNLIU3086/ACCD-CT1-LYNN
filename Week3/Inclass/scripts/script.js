bb6d3d95379ea2f23f6e29b709b1ec59d4a66ece
LYNN <18810731098@163.com>
2025-10-03 12:04:34 -0700

    week3image

---
+++ Week3/Inclass/scripts/script.js
@@ -0,0 +1,53 @@
+let cBox = document.getElementById("colorBox")
+let colorBtn = document.getElementById("changeColor")
+let imgBox = document.getElementById("QImage")
+let imageBtn = document.getElementById("toggleImage")
+
+let assignRandomColor = function ()
+
+{
+    let rComp = 255 * Math.random()
+    let gComp = 255 * Math.random()
+    let bComp = 255 * Math.random()
+
+    cBox.style.backgroundColor = "rgb(" + rComp + ", " + gComp + ", " + bComp + ")"
+}
+
+
+const toggleImage = () =>
+{
+    console.log(imgBox.src)
+    if(imgBox.src.includes("Q1"))
+    {
+       imgBox.src = "assets/Q3.png"
+    }
+}
+colorBtn.addEventListener("click", assignRandomColor)
+imageBtn.addEventListener("click", toggleImage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
