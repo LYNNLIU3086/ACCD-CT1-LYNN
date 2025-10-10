a1a50c5e4d235c890cce9d63c4caa1cea055355d
LYNN <18810731098@163.com>
2025-10-09 23:23:12 -0700

    1

---
+++ Week3/Inclass/script.js
@@ -0,0 +1,15 @@
+let gT1 = document. getElementById("galleryThumb1")
+let gT2 = document. getElementById("galleryThumb2")
+let gT3 = document. getElementById("galleryThumb3")
+let gT4 = document. getElementById("galleryThumb4")
+
+let scimage = document.getElementById("showcaseImage")
+
+function changeImage(event) {
+console.log (event)
+}
+
+gT1.addEventListener ("click", changelmage)
+gT2.addEventListener ("click", changelmage)
+gT3.addEventListener ("click", changelmage)
+gT4.addEventListener ("click", changelmage)
