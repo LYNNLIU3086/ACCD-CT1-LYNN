59fbaaae68479e66ad7ac6ac386838e65b36c455
LYNN <18810731098@163.com>
2025-10-10 12:00:32 -0700

    1200new class

--- Week4/inclass/sketch.js
+++ Week4/inclass/sketch.js
@@ -1,7 +1,40 @@
+let posX
+let posY
+
+let velX
+let velY
+
+let radius = 100
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800)
+  colorMode(HSB, width, 100,100)
+  posX = width*0.5
+  posY = height*0.5
+  velX = -2
+  velY = 3
 }
 
 function draw() {
-  background(220);
+  posX = posX +velX
+  posY += velY
+
+  if(posY + radius >= height || posY - radius <= 0){
+     velY = velY*-1
+  }
+
+    if(posX + radius >= width || posX - radius <= 0){
+     velX = velX*-1
+  }
+  background(0,0,85)
+
+  noStroke()
+  fill(posX,100,100)
+  circle(posX, posY, radius * 2);
+   
+  stroke(255,100,0)
+  strokeWeight(10)
+  fill(width*0.75,100,100)
+  rect(width*0.5 - 50,height*0.5 - 50,100,100)
 }
+ 
